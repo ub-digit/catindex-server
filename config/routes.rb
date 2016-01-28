ca32c8b96ae6c9039c49233624ce123b47255691
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-01-28 18:28:42 +0100

    Switched back route solution to work with EmberJS store find method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :session
   namespace :v1, :defaults => {:format => :json} do
     resources :users
-    get 'cards', to: 'cards#show'
+    get 'cards/:registration_type', to: 'cards#show'
   end
 end
